3f29f708844c1c43749bb9ca275dbc5d4e37acd2
Pratik Kamble <pratikkamble700@gmail.com>
2019-11-27 02:35:44 +0530

    Navigation jetpack component integrated

--- app/src/main/java/com/pratik/shweta/ui/main/MainActivity.java
+++ app/src/main/java/com/pratik/shweta/ui/main/MainActivity.java
@@ -2,12 +2,19 @@ package com.pratik.shweta.ui.main;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
 import com.pratik.shweta.BaseActivity;
 import com.pratik.shweta.R;
 import com.pratik.shweta.SessionManager;
@@ -16,9 +23,11 @@ import com.pratik.shweta.ui.main.profile.ProfileFragment;
 
 import javax.inject.Inject;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Inject
     SessionManager sessionManager;
@@ -27,30 +36,88 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testFragmentTransaction();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        init();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater mainMenuInflater = getMenuInflater();
-        mainMenuInflater.inflate(R.menu.main_menu,menu);
+        mainMenuInflater.inflate(R.menu.main_menu, menu);
         return true;
     }
 
-    private void testFragmentTransaction(){
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_container,new PostsFragment())
-                .commit();
+    private void init() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout);
+        NavigationUI.setupWithNavController(navigationView, navController);// it will provide animationfor nav icon
+        navigationView.setNavigationItemSelectedListener(this);
     }
-
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_logout:
                 sessionManager.logOut();
                 return true;
+
+            //Hamberger close drawer
+            case android.R.id.home:
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                    return true;
+                } else
+                    return false;// return false means not counsider click
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_profile:{
+
+                NavOptions navOptions = new NavOptions.Builder()
+                        .setPopUpTo(R.id.main, true)
+                        .build();
+
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(
+                        R.id.profileScreen,
+                        null,
+                        navOptions
+                );
+
+                break;
+            }
+
+            case R.id.nav_posts:{
+                // do you this if current fragment openagain and again then it should not be added in backstack.
+                // transacton should not happen
+                if(isValidDestination(R.id.postsScreen)){
+                    Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.postsScreen);
+                }
+
+                break;
+            }
+        }
+
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private boolean isValidDestination(int destination){
+        return destination != Navigation.findNavController(this, R.id.nav_host_fragment).getCurrentDestination().getId();
+    }
+
+    //hamberger open work using this method
+    //Enable back arrow
+    @Override
+    public boolean onSupportNavigateUp() {
+        return NavigationUI.navigateUp(Navigation.findNavController(this, R.id.nav_host_fragment), drawerLayout);
     }
 }
